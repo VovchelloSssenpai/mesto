85b96f5ee0d464765883f604ef07ef0f41eebc7e
Vladimir Mkhitaryan <vova.mkhitaryan94@gmail.com>
2022-12-25 18:02:05 +0400

    v1

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+// DOM Elements
+
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__close-button");
+const popup = document.querySelector(".popup");
+const profileUsername = document.querySelector(".profile__user-name");
+const profileUserprofession = document.querySelector(
+  ".profile__user-profession"
+);
+const popupNameInput = document.querySelector(".popup__name-input");
+const popupProfessionInput = document.querySelector(".popup__profession-input");
+const popupSubmitButton = document.querySelector(".popup__submit-button");
+const submitForm = document.querySelector(".popup__form");
+
+// Functions
+
+const popupClose = function () {
+  popup.classList.remove("popup_opened");
+};
+
+const popupOpen = function () {
+  popup.classList.add("popup_opened");
+};
+
+// Open, Close Popup
+editButton.addEventListener("click", () => {
+  popupOpen();
+  popupNameInput.value = profileUsername.textContent;
+  popupProfessionInput.value = profileUserprofession.textContent;
+  console.log(profileUserprofession.textContent);
+});
+
+closeButton.addEventListener("click", popupClose);
+
+// Editing form
+const usernameEditing = function (evt) {
+  evt.preventDefault();
+
+  const newUserContent = popupNameInput.value;
+  profileUsername.textContent = newUserContent;
+
+  const newProfessionContent = popupProfessionInput.value;
+  profileUserprofession.textContent = newProfessionContent;
+
+  popupClose();
+};
+
+submitForm.addEventListener("submit", usernameEditing);
